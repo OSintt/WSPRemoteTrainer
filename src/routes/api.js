1dcccb9535e52b423ce69d638e2e4d160225536c
osintt <osint@gmail.com>
2023-05-30 16:16:54 +0000

    creating listen route and optimizing answers

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.use('/train', require('./training.routes'));
+router.use('/t', require('./training.routes'));
 
 module.exports = router; 
--- src/routes/training.routes.js
+++ src/routes/training.routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { training } from '../controllers/training';
+import { training, listen } from '../controllers/training';
 
 const router = Router();
 router.get('/', training);
+router.get('/listen', listen);
 
 module.exports = router;
