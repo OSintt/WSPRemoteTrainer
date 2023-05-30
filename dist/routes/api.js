186c3ce7f6aa127995e4ea85172b15b1454ed33f
OSint <osintaxis@gmail.com>
2023-05-30 09:07:51 -0500

    🐤

--- dist/routes/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-const _express = require("express");
-const router = (0, _express.Router)();
-router.use('/train', require('./training.routes'));
-module.exports = router;
--- dist/routes/training.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-const _express = require("express");
-const _training = require("../controllers/training");
-const router = (0, _express.Router)();
-router.get('/', _training.training);
-module.exports = router;
