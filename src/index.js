b9543a733c30341dd04d8ffd6ed8334bac9a7a5e
OSint <osintaxis@gmail.com>
2023-06-01 12:03:38 -0500

    uh

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './db';
 import app from './app';
 //import { createApiKey, createBots } from './lib/createData';
-import Bot from './models/Bot';
 const main = async () => {
-    console.log(await Bot.find());
     app.listen(app.get('port'), () => console.log('Server running on port', app.get('port'))); 
     //createApiKey();
     //createBots(); 
