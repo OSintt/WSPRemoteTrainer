e5aef0a179999a5d8d16fc98c6accda81efe91d4
OSint <osintaxis@gmail.com>
2023-06-01 12:12:12 -0500

    fixing api key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './db';
 import app from './app';
 //import { createApiKey, createBots } from './lib/createData';
+
 const main = async () => {
     app.listen(app.get('port'), () => console.log('Server running on port', app.get('port'))); 
     //createApiKey();
