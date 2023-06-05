39a8f6fa32ad1d43d1bad1bb5dfaaa6bda44d373
OSint <osintaxis@gmail.com>
2023-06-05 11:53:03 -0500

    o.o

--- src/routes/training.routes.js
+++ src/routes/training.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { training, listen, campaign } from '../controllers/training';
 import { checkKey } from '../middleware/checkReq';
 const router = Router();
-router.get('/', [checkKey],  training);
+router.get('/',  training);
 router.get('/listen', [checkKey], listen);
 router.get('/campaign', [checkKey], campaign);
 
